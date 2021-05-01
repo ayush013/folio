ac3b207c7d1bcb731ad26a71a2364ea77f0f0aa7
Ayush Singh <ayush.singh.xda@gmail.com>
2021-05-01 18:17:33 +0530

    Product Sans font family added

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -5,8 +5,7 @@ const Header = ({ children }) => {
         <nav className='w-full py-8 fixed top-0 2xl:container mx-auto xl:px-20 md:px-12 px-4'>
             <div className='relative flex justify-between'>
                 <a href='#home' className='flex gap-3 items-center link'>
-                    <Image src='/logo.svg' alt='Logo - Ayush Singh' width={15} height={15} />
-                    <span className='font-bold uppercase tracking-wide'>AYUSH SINGH</span>
+                    <Image src='/logo.svg' alt='Logo - Ayush Singh' width={22} height={22} />
                 </a>
                 <div className='outer-menu'>
                     <input className='checkbox-toggle link absolute top-0 right-0 w-6 h-6 opacity-0' type='checkbox' />
