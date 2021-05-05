b1e6ad1683413be0ff29342a45a12a3693116cb1
Ayush Singh <ayush.singh.xda@gmail.com>
2021-05-05 17:47:32 +0530

    GTAG Script moved to head

--- components/common/scripts.tsx
+++ components/common/scripts.tsx
@@ -1,39 +1,35 @@
-import { useEffect } from 'react';
+import Head from 'next/head';
 import { GTAG } from '../../constants';
 
 const Scripts = () => {
 
-    useEffect(() => {
-        const gtag = document.createElement('script');
-        gtag.src = `https://www.googletagmanager.com/gtag/js?id=${GTAG}`;
-        gtag.type = 'text/javascript'
-        gtag.async = true;
-
-        const gtagExec = document.createElement('script');
-        gtagExec.innerHTML = `window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-        gtag('config', '${GTAG}');`;
-
-        
-        const chaport = document.createElement('script');
-        chaport.innerHTML = `setTimeout(() => {
-            (function (w, d, v3) {
-                w.chaportConfig = {
-                    appId: '5ec95b5d1db3487e521123ca'
-                };
-                if (w.chaport) return; v3 = w.chaport = {}; v3._q = []; v3._l = {}; v3.q = function () { v3._q.push(arguments) }; v3.on = function (e, fn) { if (!v3._l[e]) v3._l[e] = []; v3._l[e].push(fn) }; var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://app.chaport.com/javascripts/insert.js'; var ss = d.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss)
-            })(window, document);
-        }, 10000);`;
-
-        document.body.appendChild(gtag);
-        document.body.appendChild(gtagExec);
-        document.body.appendChild(chaport);
-
-    }, [])
-
     return (
         <>
+            <Head>
+                <script src={`https://www.googletagmanager.com/gtag/js?id=${GTAG}`} async></script>
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag() { dataLayer.push(arguments); }
+                    gtag('js', new Date());
+            
+                    gtag('config', '${GTAG}');
+            `}}></script>
+            </Head>
+
+            <script dangerouslySetInnerHTML={{
+                __html: `
+                    setTimeout(() => {
+                        (function (w, d, v3) {
+                            w.chaportConfig = {
+                                appId: '5ec95b5d1db3487e521123ca'
+                            };
+            
+                            if (w.chaport) return; v3 = w.chaport = {}; v3._q = []; v3._l = {}; v3.q = function () { v3._q.push(arguments) }; v3.on = function (e, fn) { if (!v3._l[e]) v3._l[e] = []; v3._l[e].push(fn) }; var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://app.chaport.com/javascripts/insert.js'; var ss = d.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss)
+                        })(window, document);
+                    }, 10000);
+                `}}></script>
+
         </>
     )
 }
