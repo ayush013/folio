7bdeccff517db81eb78265deaa9abeeec43a9902
Ayush Singh <ayush.singh.xda@gmail.com>
2021-09-01 21:46:07 +0530

    Configured eslint, memoized scripts, hero components

--- components/common/scripts.tsx
+++ components/common/scripts.tsx
@@ -1,38 +1,38 @@
-import Script from 'next/script';
-import { GTAG } from '../../constants';
+import Script from "next/script";
+import React from "react";
+import { GTAG } from "../../constants";
 
-const Scripts = () => {
-
-    return (
-        <>
-            <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${GTAG}`}
-                strategy="afterInteractive"
-            />
-            <Script strategy="afterInteractive">
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag() { dataLayer.push(arguments); }
-                    gtag('js', new Date());
-            
-                    gtag('config', '${GTAG}');
-                `}
-            </Script>
-            <Script strategy="lazyOnload">
-                {`
-                    setTimeout(() => {
-                        (function (w, d, v3) {
-                            w.chaportConfig = {
-                                appId: '5ec95b5d1db3487e521123ca'
-                            };
-            
-                            if (w.chaport) return; v3 = w.chaport = {}; v3._q = []; v3._l = {}; v3.q = function () { v3._q.push(arguments) }; v3.on = function (e, fn) { if (!v3._l[e]) v3._l[e] = []; v3._l[e].push(fn) }; var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://app.chaport.com/javascripts/insert.js'; var ss = d.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss)
-                        })(window, document);
-                    }, 15000);
-                `}
-            </Script>
-        </>
-    )
-}
+const Scripts = React.memo(() => {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GTAG}`}
+        strategy="afterInteractive"
+      />
+      <Script strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+    
+            gtag('config', '${GTAG}');
+        `}
+      </Script>
+      <Script strategy="lazyOnload">
+        {`
+            setTimeout(() => {
+                (function (w, d, v3) {
+                    w.chaportConfig = {
+                        appId: '5ec95b5d1db3487e521123ca'
+                    };
+    
+                    if (w.chaport) return; v3 = w.chaport = {}; v3._q = []; v3._l = {}; v3.q = function () { v3._q.push(arguments) }; v3.on = function (e, fn) { if (!v3._l[e]) v3._l[e] = []; v3._l[e].push(fn) }; var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://app.chaport.com/javascripts/insert.js'; var ss = d.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss)
+                })(window, document);
+            }, 15000);
+        `}
+      </Script>
+    </>
+  );
+});
 
 export default Scripts;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,51 @@
-import { METADATA } from '../constants'
-import Head from 'next/head'
-import { useEffect, useState } from 'react';
+import { METADATA } from "../constants";
+import Head from "next/head";
+import { useEffect, useState } from "react";
 
-import Layout from '@/components/common/layout'
-import Header from '@/components/common/header'
-import Menu from '@/components/common/menu'
-import ProgressIndicator from '@/components/common/progress-indicator'
-import Cursor from '@/components/common/cursor'
-import Hero from '@/components/home/hero';
-import Projects from '@/components/home/projects';
-import Quote from '@/components/home/quote';
-import Skills from '@/components/home/skills';
-import Collaboration from '@/components/home/collaboration';
-import Footer from '@/components/common/footer';
-import Timeline from '@/components/home/timeline';
-import Scripts from '@/components/common/scripts';
+import Layout from "@/components/common/layout";
+import Header from "@/components/common/header";
+import Menu from "@/components/common/menu";
+import ProgressIndicator from "@/components/common/progress-indicator";
+import Cursor from "@/components/common/cursor";
+import Hero from "@/components/home/hero";
+import Projects from "@/components/home/projects";
+import Quote from "@/components/home/quote";
+import Skills from "@/components/home/skills";
+import Collaboration from "@/components/home/collaboration";
+import Footer from "@/components/common/footer";
+import Timeline from "@/components/home/timeline";
+import Scripts from "@/components/common/scripts";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { gsap } from "gsap";
 
 export default function Home() {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.config({ nullTargetWarn: false });
 
   const [isDesktop, setisDesktop] = useState(true);
   const [clientHeight, setHeight] = useState(0);
 
   useEffect(() => {
-    const result = (typeof window.orientation === 'undefined') && (navigator.userAgent.indexOf('IEMobile') === -1);
-    window.history.scrollRestoration = 'manual';
+    let timer = null;
+    const callback = () => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        const result =
+          typeof window.orientation === "undefined" &&
+          navigator.userAgent.indexOf("IEMobile") === -1;
+        window.history.scrollRestoration = "manual";
+        setisDesktop(result);
+        setHeight(window.innerHeight);
+      }, 100);
+    };
 
-    setisDesktop(result);
-    setHeight(window.innerHeight);
+    callback();
 
-  }, [isDesktop, clientHeight]);
+    window.addEventListener("resize", callback);
+    return () => {
+      window.removeEventListener("resize", callback);
+    };
+  }, []);
 
   return (
     <>
@@ -37,22 +54,22 @@ export default function Home() {
       </Head>
       <Layout>
         <Header>
-          <Menu></Menu>
+          <Menu />
         </Header>
-        <ProgressIndicator></ProgressIndicator>
-        <Cursor isDesktop={isDesktop}></Cursor>
-        <main className='flex-col flex'>
-          <div className='fixed top-0 left-0 h-screen w-screen bg-gray-900 -z-1'></div>
-          <Hero></Hero>
-          <Projects clientHeight={clientHeight}></Projects>
-          <Quote clientHeight={clientHeight}></Quote>
-          <Skills></Skills>
-          <Timeline isDesktop={isDesktop}></Timeline>
-          <Collaboration clientHeight={clientHeight}></Collaboration>
-          <Footer></Footer>
+        <ProgressIndicator />
+        <Cursor isDesktop={isDesktop} />
+        <main className="flex-col flex">
+          <div className="fixed top-0 left-0 h-screen w-screen bg-gray-900 -z-1"></div>
+          <Hero />
+          <Projects clientHeight={clientHeight} />
+          <Quote clientHeight={clientHeight} />
+          <Skills />
+          <Timeline isDesktop={isDesktop} />
+          <Collaboration clientHeight={clientHeight} />
+          <Footer />
         </main>
-        <Scripts></Scripts>
+        <Scripts />
       </Layout>
     </>
-  )
+  );
 }
