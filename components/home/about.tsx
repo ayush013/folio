3ffecbcbc53eaea8edd34e811020581ec248deb2
Ayush Singh <ayush.singh.xda@gmail.com>
2022-04-02 15:10:09 +0530

    about component refactor

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -1,12 +1,15 @@
 import { gsap, Linear } from "gsap";
-import { MutableRefObject, useEffect, useRef } from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 const AboutSection = ({ clientHeight }) => {
   const quoteRef: MutableRefObject<HTMLDivElement> = useRef(null);
   const targetSection: MutableRefObject<HTMLDivElement> = useRef(null);
 
-  useEffect(() => {
+  const initAboutAnimation = (
+    quoteRef: MutableRefObject<HTMLDivElement>,
+    targetSection: MutableRefObject<HTMLDivElement>
+  ): ScrollTrigger => {
     const timeline = gsap.timeline({
       defaults: { ease: Linear.easeNone, duration: 0.1 },
     });
@@ -31,38 +34,46 @@ const AboutSection = ({ clientHeight }) => {
         delay: 1,
       });
 
-    ScrollTrigger.create({
+    const scrollTriggerInstance = ScrollTrigger.create({
       trigger: targetSection.current,
       start: "center 80%",
       end: "center top",
       scrub: 0,
       animation: timeline,
     });
+    return scrollTriggerInstance;
+  };
+
+  useEffect(() => {
+    const aboutScrollTriggerInstance = initAboutAnimation(
+      quoteRef,
+      targetSection
+    );
+
+    return aboutScrollTriggerInstance.kill;
   }, [quoteRef, targetSection]);
 
+  const renderQuotes = (): React.ReactNode => (
+    <h1 ref={quoteRef} className="font-medium text-3xl sm:text-4xl md:text-6xl">
+      <span className="about-1 leading-tight">
+        I am a passionate UI Engineer who bridges the gap between development
+        and design.{" "}
+      </span>
+      <span className="about-2 leading-tight">
+        I take responsibility to craft a good user experience using modern
+        frontend architecture.
+      </span>
+    </h1>
+  );
+
   return (
-    <section className="w-full relative select-none" ref={targetSection}>
-      <div
-        className={`
-          ${
-            clientHeight > 650 ? "pt-20 pb-16" : "pt-40 pb-24"
-          } section-container
-        `}
-      >
-        <h1
-          ref={quoteRef}
-          className="font-medium text-3xl sm:text-4xl md:text-6xl"
-        >
-          <span className="about-1 leading-tight">
-            I am a passionate UI Engineer who bridges the gap between
-            development and design.{" "}
-          </span>
-          <span className="about-2 leading-tight">
-            I take responsibility to craft a good user experience using modern
-            frontend architecture.{" "}
-          </span>
-        </h1>
-      </div>
+    <section
+      className={`${
+        clientHeight > 650 ? "pt-20 pb-16" : "pt-40 pb-24"
+      } w-full relative select-none section-container`}
+      ref={targetSection}
+    >
+      {renderQuotes()}
     </section>
   );
 };
