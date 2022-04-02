ca9bba82bcc9ce68ba13df5f6529c3b86e66f388
Ayush Singh <ayush.singh.xda@gmail.com>
2022-04-02 16:21:13 +0530

    refactored projects and quote section

--- components/home/projects.tsx
+++ components/home/projects.tsx
@@ -1,47 +1,29 @@
-import { MutableRefObject, useEffect, useRef } from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
 import { MENULINKS, PROJECTS } from "../../constants";
 import ProjectTile from "../common/project-tile";
 import { gsap, Linear } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
+const PROJECT_STYLES = {
+  SECTION:
+    "w-full relative select-none section-container transform-gpu flex-col flex py-8 justify-center",
+  PROJECTS_WRAPPER: "tall:mt-12 mt-6 flex project-wrapper w-fit seq",
+};
+
 const ProjectsSection = ({ isDesktop }: { isDesktop: boolean }) => {
-  const targetSection: MutableRefObject<HTMLDivElement> = useRef(null);
-  const sectionTitle: MutableRefObject<HTMLDivElement> = useRef(null);
+  const targetSectionRef: MutableRefObject<HTMLDivElement> = useRef(null);
+  const sectionTitleElementRef: MutableRefObject<HTMLDivElement> = useRef(null);
 
-  useEffect(() => {
-    let projectsScrollTrigger;
-    let projectsTimeline;
-
-    if (isDesktop) {
-      [projectsTimeline, projectsScrollTrigger] = getProjectsSt();
-    } else {
-      const projectWrapper = targetSection.current.querySelector(
-        ".project-wrapper"
-      ) as HTMLDivElement;
-      projectWrapper.style.width = "calc(100vw - 1rem)";
-      projectWrapper.style.overflowX = "scroll";
-    }
-
-    const [revealTimeline, revealScrollTrigger] = getRevealSt();
-
-    return () => {
-      projectsScrollTrigger && projectsScrollTrigger.kill();
-      projectsTimeline && projectsTimeline.kill();
-      revealScrollTrigger && revealScrollTrigger.kill();
-      revealTimeline && revealTimeline.progress(1);
-    };
-  }, [targetSection, sectionTitle, isDesktop]);
-
-  const getRevealSt = (): [GSAPTimeline, ScrollTrigger] => {
+  const initRevealAnimation = (): [GSAPTimeline, ScrollTrigger] => {
     const revealTl = gsap.timeline({ defaults: { ease: Linear.easeNone } });
     revealTl.from(
-      targetSection.current.querySelectorAll(".seq"),
+      targetSectionRef.current.querySelectorAll(".seq"),
       { opacity: 0, duration: 0.5, stagger: 0.5 },
       "<"
     );
 
     const scrollTrigger = ScrollTrigger.create({
-      trigger: targetSection.current,
+      trigger: targetSectionRef.current,
       start: "top bottom",
       end: "bottom bottom",
       scrub: 0,
@@ -51,23 +33,23 @@ const ProjectsSection = ({ isDesktop }: { isDesktop: boolean }) => {
     return [revealTl, scrollTrigger];
   };
 
-  const getProjectsSt = (): [GSAPTimeline, ScrollTrigger] => {
+  const initProjectsAnimation = (): [GSAPTimeline, ScrollTrigger] => {
     const timeline = gsap.timeline({ defaults: { ease: Linear.easeNone } });
     const sidePadding =
       document.body.clientWidth -
-      targetSection.current.querySelector(".inner-container").clientWidth;
+      targetSectionRef.current.querySelector(".inner-container").clientWidth;
     const elementWidth =
       sidePadding +
-      targetSection.current.querySelector(".project-wrapper").clientWidth;
-    targetSection.current.style.width = `${elementWidth}px`;
+      targetSectionRef.current.querySelector(".project-wrapper").clientWidth;
+    targetSectionRef.current.style.width = `${elementWidth}px`;
     const width = window.innerWidth - elementWidth;
     const duration = `${(elementWidth / window.innerHeight) * 100}%`;
     timeline
-      .to(targetSection.current, { x: width })
-      .to(sectionTitle.current, { x: -width }, "<");
+      .to(targetSectionRef.current, { x: width })
+      .to(sectionTitleElementRef.current, { x: -width }, "<");
 
     const scrollTrigger = ScrollTrigger.create({
-      trigger: targetSection.current,
+      trigger: targetSectionRef.current,
       start: "top top",
       end: duration,
       scrub: 0,
@@ -79,46 +61,70 @@ const ProjectsSection = ({ isDesktop }: { isDesktop: boolean }) => {
     return [timeline, scrollTrigger];
   };
 
+  useEffect(() => {
+    let projectsScrollTrigger: ScrollTrigger | undefined;
+    let projectsTimeline: GSAPTimeline | undefined;
+
+    if (isDesktop) {
+      [projectsTimeline, projectsScrollTrigger] = initProjectsAnimation();
+    } else {
+      const projectWrapper = targetSectionRef.current.querySelector(
+        ".project-wrapper"
+      ) as HTMLDivElement;
+      projectWrapper.style.width = "calc(100vw - 1rem)";
+      projectWrapper.style.overflowX = "scroll";
+    }
+
+    const [revealTimeline, revealScrollTrigger] = initRevealAnimation();
+
+    return () => {
+      projectsScrollTrigger && projectsScrollTrigger.kill();
+      projectsTimeline && projectsTimeline.kill();
+      revealScrollTrigger && revealScrollTrigger.kill();
+      revealTimeline && revealTimeline.progress(1);
+    };
+  }, [targetSectionRef, sectionTitleElementRef, isDesktop]);
+
+  const renderSectionTitle = (): React.ReactNode => (
+    <div
+      className="flex flex-col inner-container transform-gpu"
+      ref={sectionTitleElementRef}
+    >
+      <p className="uppercase tracking-widest text-gray-200 text-sm seq">
+        PROJECTS
+      </p>
+      <h1 className="md:text-5xl text-4xl font-bold text-gradient seq w-fit mt-2">
+        My Works
+      </h1>
+      <h2 className="text-2xl md:max-w-3xl w-full seq max-w-sm mt-2">
+        I have contributed in over 20+ projects ranging from Frontend
+        development, UI/UX design, Open Source, and Motion Graphics
+      </h2>
+    </div>
+  );
+
+  const renderProjectTiles = (): React.ReactNode =>
+    PROJECTS.map((project, idx) => (
+      <ProjectTile
+        classes={(idx !== PROJECTS.length - 1 || !isDesktop) && "md:mr-10 mr-6"}
+        project={project}
+        key={project.name}
+        isDesktop={isDesktop}
+      ></ProjectTile>
+    ));
+
+  const { ref: projectsSectionRef } = MENULINKS[1];
+
   return (
     <section
-      ref={targetSection}
-      className={`${
-        isDesktop && "min-h-screen"
-      } w-full relative select-none section-container transform-gpu`}
-      id={MENULINKS[1].ref}
+      ref={targetSectionRef}
+      className={`${isDesktop && "min-h-screen"} ${PROJECT_STYLES.SECTION}`}
+      id={projectsSectionRef}
     >
-      <div className="flex-col flex py-8 justify-center h-full">
-        <div
-          className="flex flex-col inner-container transform-gpu"
-          ref={sectionTitle}
-        >
-          <p className="uppercase tracking-widest text-gray-200 text-sm seq">
-            PROJECTS
-          </p>
-          <h1 className="md:text-5xl text-4xl font-bold text-gradient seq w-fit mt-2">
-            My Works
-          </h1>
-          <h2 className="text-2xl md:max-w-3xl w-full seq max-w-sm mt-2">
-            I have contributed in over 20+ projects ranging from Frontend
-            development, UI/UX design, Open Source, and Motion Graphics
-          </h2>
-        </div>
-        <div className="tall:mt-12 mt-6 flex project-wrapper w-fit seq">
-          {PROJECTS.map((project, idx) => (
-            <ProjectTile
-              classes={idx !== PROJECTS.length - 1 && "md:mr-10 mr-6"}
-              project={project}
-              key={project.name}
-              isDesktop={isDesktop}
-            ></ProjectTile>
-          ))}
-        </div>
+      {renderSectionTitle()}
+      <div className={PROJECT_STYLES.PROJECTS_WRAPPER}>
+        {renderProjectTiles()}
       </div>
-      <style jsx global>{`
-        .project-wrapper::-webkit-scrollbar {
-          display: none;
-        }
-      `}</style>
     </section>
   );
 };
--- components/home/quote.tsx
+++ components/home/quote.tsx
@@ -1,12 +1,12 @@
 import { gsap, Linear } from "gsap";
-import { MutableRefObject, useEffect, useRef } from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 const QuoteSection = () => {
   const quoteRef: MutableRefObject<HTMLDivElement> = useRef(null);
   const targetSection: MutableRefObject<HTMLDivElement> = useRef(null);
 
-  useEffect(() => {
+  const initQuoteAnimation = (): ScrollTrigger => {
     const timeline = gsap.timeline({ defaults: { ease: Linear.easeNone } });
     timeline
       .from(quoteRef.current, { opacity: 0, duration: 2 })
@@ -15,40 +15,36 @@ const QuoteSection = () => {
         duration: 1,
       });
 
-    ScrollTrigger.create({
+    return ScrollTrigger.create({
       trigger: targetSection.current,
       start: "center bottom",
       end: "center center",
       scrub: 0,
       animation: timeline,
     });
+  };
+
+  useEffect(() => {
+    const quoteAnimationRef = initQuoteAnimation();
+
+    return quoteAnimationRef.kill;
   }, [quoteRef, targetSection]);
+
+  const renderQuote = (): React.ReactNode => (
+    <div className="tall:py-60 py-72 section-container">
+      <h1
+        ref={quoteRef}
+        className="font-medium text-4xl md:text-5xl text-center"
+      >
+        I have a <span className="text-strong font-bold">strong</span> obsession
+        for attention to detail.
+      </h1>
+    </div>
+  );
 
   return (
     <section className="w-full relative select-none" ref={targetSection}>
-      <div className="tall:py-60 py-72 section-container">
-        <h1
-          ref={quoteRef}
-          className="font-medium text-4xl md:text-5xl text-center"
-        >
-          I have a <span className="text-strong font-bold">strong</span>{" "}
-          obsession for attention to detail.
-        </h1>
-      </div>
-      <style jsx global>{`
-        .text-strong {
-          background: linear-gradient(
-            90deg,
-            #ffffff 0%,
-            #ffffff 50%,
-            #6dd5ed 51%,
-            #2193b0 100%
-          );
-          background-size: 200% 100%;
-          -webkit-background-clip: text;
-          -webkit-text-fill-color: transparent;
-        }
-      `}</style>
+      {renderQuote()}
     </section>
   );
 };
